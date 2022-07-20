0f1d250adaebb3f15043b696491fa28157b8a9ab
Thiago Doval <thiagodovalaraujo@gmail.com>
2022-07-19 21:29:03 -0300

    Alert Reducer and Alert Component

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Alert from './components/layout/Alert';
 
 // Redux
 import { Provider } from 'react-redux';
@@ -21,6 +22,7 @@ const App = () => (
           <Route exact path='/' element={<Landing />} />
         </Routes>
         <section className='container'>
+          <Alert />
           <Routes>
             <Route exact path='/register' element={<Register />} />
             <Route exact path='/login' element={<Login />} />
